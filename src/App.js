1f0316b0d96a8c02b65f071292685eda6e4d194a
Anatoliy_Iliev <vekok12@gmail.com>
2021-07-26 18:02:09 +0300

    day-2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-
-
-function App() {
-  return (
-    <div>
-    
-    </div>
-  );
+import React, { Component } from "react";
+import { v4 as uuidv4 } from 'uuid';
+class App extends Component {
+  state = {
+    contacts: [],
+    name: ''
+  }
+  
+  handleNameChange = event => {
+    console.log(event.currentTarget);
+    // this.setState({ name: event.currentTarget.value });
+  }
+  
+  
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <form>
+          <label>
+            Name
+            <input
+              type="text"
+              value={this.state.name}
+              onChange={this.handleInputChange}
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+              required
+              key = { uuidv4() }
+            />
+            
+          </label>
+        </form>
+        
+        <button>Add contact</button>
+        <h2>Contacts</h2>
+        <ul></ul>
+      
+      </div>
+    );
+  }
+  
 }
 
 export default App;
